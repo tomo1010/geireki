8fbed248cff36afea3bf016c42bd2c534ca7e8b7
toto <toto.techa@gmail.com>
2021-07-14 13:07:11 +0000

    deploy2

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,27 @@ Route::get('perfomers/{id}', 'PerfomersController@show')->name('perfomers.show')
 
 
 
+    //csv処理
+    // 芸人データ 
+    Route::get('csv/entertainer', 'CsvController@uploadEntertainer');
+    Route::post('csv/entertainer', 'CsvController@importEntertainer')->name('csv.importEntertainer');
+    
+    // 事務所データ 
+    Route::get('csv/office', 'CsvController@uploadOffice');
+    Route::post('csv/office', 'CsvController@importOffice')->name('csv.importOffice');
+    
+    // 個人データ 
+    Route::get('csv/perfomer', 'CsvController@uploadPerfomer');
+    Route::post('csv/perfomer', 'CsvController@importPerfomer')->name('csv.importPerfomer');
+
+    // 芸人個人（中間）データ 
+    Route::get('csv/member', 'CsvController@uploadMember');
+    Route::post('csv/member', 'CsvController@importMember')->name('csv.importMember');
+    
+
+
+
+
 // signup
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');

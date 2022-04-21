32a9baa58f9b121b2903f525abce6192b75562b2
tomo1010 <kjktomo@gmail.com>
2022-04-21 13:02:54 +0000

    age

--- resources/views/lists/ageList.blade.php
+++ resources/views/lists/ageList.blade.php
@@ -7,7 +7,6 @@
 
     <div class="container">
         <div class="row">
-
                 <table class="table table-striped">
                     <thead>
                         <tr>
@@ -19,71 +18,26 @@
                     </thead>
                     <tbody>
                         @foreach ($perfomers as $perfomer)
-                        @if($perfomer->activeend == NULL){{--解散済みの場合はグレー文字--}}
-                        <tr>
-                            {{--名前リンク--}}
-                            <td nowrap>@include('commons.perfomer_name')</td>
-
-                            {{--コンビ名リンク--}}
-                            @empty($perfomer->entertainer[0]->name)
-                            <td>-</td>
+                            @if($perfomer->activeend == NULL){{--解散済みの場合はグレー文字--}}
+                            <tr>
+                                {{--名前リンク--}}
+                                <td nowrap>@include('commons.perfomer_name')</td>
+                                @include('lists.age_common')
+                            </tr>
                             @else
-                                <td>@include('commons.perfomer_combiName')</td>
-                            @endempty
-                            
-                            {{--年齢リンク--}}
-                            @empty($perfomer->birthday)
-                            <td>-</td>
-                            @else
-                            <td>@include('commons.perfomer_age')歳</td>
-                            @endempty
-
-                            {{--芸歴リンク--}}                            
-                            @empty($perfomer->active)
-                            <td>-</td>
-                            @else
-                            <td>@include('commons.perfomer_history')年</td>
-                            @endempty
-                        </tr>
-                        @else
-                        <tr class="text-secondary">
-                            <td nowrap>{!! link_to_route('perfomers.show', $perfomer->name, [$perfomer->id]) !!}（解散済）</td>
-                            
-                            {{--コンビ名もリンク--}}
-                            <td>
-                            @if(!empty($perfomer->entertainer[0]->name))
-                                {!! link_to_route('entertainers.show', $perfomer->entertainer[0]->name, $perfomer->entertainer[0]->id) !!}
-                            @else
-                            @endif    
-                            </td>
-                            
-                            {{--年齢もリンク--}}
-                            @empty($perfomer->birthday)
-                            <td>-</td>
-                            @else
-                            <td>{!! link_to_route('lists.age2List', $now->diffInYears($perfomer->birthday), ['yearsOld' => $now->diffInYears($perfomer->birthday)]) !!}歳</td>
-                            @endempty
-
-                            {{--芸歴もリンク--}}                            
-                            @empty($perfomer->active)
-                            <td>-</td>
-                            @else
-                            <td>{!! link_to_route('lists.historyList', $now->diffInYears($perfomer->active), ['year' => $now->diffInYears($perfomer->active)]) !!}年</td>
-                            @endempty
-                        </tr>
-                        @endif
+                            <tr class="text-secondary">
+                                {{--名前リンク（解散済み）--}}
+                                <td nowrap>@include('commons.perfomer_name')（解散済）</td>                            
+                                @include('lists.age_common')
+                            </tr>
+                            @endif
                         @endforeach
                     </tbody>   
                 </table>
-
-
         </div>
     </div>
 
-
-
     {{-- ページネーションのリンク --}}
-
-        {{ $perfomers->appends(request()->query())->links() }}        
+    {{ $perfomers->appends(request()->query())->links() }}        
 
 @endsection
